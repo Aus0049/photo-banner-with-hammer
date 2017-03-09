69a9f99471108c3c26e604e13e2d59366b9b73b9
赵彬宇 <zhaobinyu@qijiayoudao.com>
2017-03-09 12:00:24 +0800

    第三步 增加闭包

--- js/index.js
+++ js/index.js
@@ -23,59 +23,190 @@
 // step 2 增加左右滑动 swipe切换
 // 需求 对着banner 左右滑动 完成切换 不循环切换
 // 使用hammer的 滑动swipeleft swiperight函数完成
-var swipeListHammer = new Hammer($("#swipe-list")[0]);
-var header = $(".header-list");
-var headerList = $(".header-list li").hammer();
-var footerList = $(".foot span");
-var banner = $("#swipe-list");
-var currentBannerIndex = 0; // 记录当前显示的那个tab
+// var swipeListHammer = new Hammer($("#swipe-list")[0]);
+// var header = $(".header-list");
+// var headerList = $(".header-list li").hammer();
+// var footerList = $(".foot span");
+// var banner = $("#swipe-list");
+// var currentBannerIndex = 0; // 记录当前显示的那个tab
 
-// 点击滑动
-headerList.on("tap", function(){
-    var index = $(this).index();
+// // 点击滑动
+// headerList.on("tap", function(){
+//     var index = $(this).index();
 
-    banner.animate({marginLeft: -(8.53333 * index) + "rem" }, 500);
+//     banner.animate({marginLeft: -(8.53333 * index) + "rem" }, 500);
 
-    footerList.removeClass("active").eq(index).addClass("active");
+//     footerList.removeClass("active").eq(index).addClass("active");
 
-    headerList.removeClass("active").eq(index).addClass("active");
+//     headerList.removeClass("active").eq(index).addClass("active");
 
-    if([2,3,4].indexOf(index) >= 0){
-        header.animate({marginLeft: -(1.46667 * (index - 1)) + "rem"});
-    }
-    currentBannerIndex = index;
-});
-// ←滑 banner向左走 margin-left 减小
-swipeListHammer.on("swipeleft", function(e){
-    // 最后一张 不可再右滑 所以是<5
-    if(currentBannerIndex < 5){
-        banner.animate({marginLeft: -(8.53333 * (currentBannerIndex + 1)) + "rem" }, 500);
-        // foot
-        footerList.removeClass("active").eq(currentBannerIndex + 1).addClass("active");
-        // header滑动
-        headerList.removeClass("active").eq(currentBannerIndex + 1).addClass("active");
+//     if([2,3,4].indexOf(index) >= 0){
+//         header.animate({marginLeft: -(1.46667 * (index - 1)) + "rem"});
+//     }
+//     currentBannerIndex = index;
+// });
+// // ←滑 banner向左走 margin-left 减小
+// swipeListHammer.on("swipeleft", function(e){
+//     // 最后一张 不可再右滑 所以是<5
+//     if(currentBannerIndex < 5){
+//         banner.animate({marginLeft: -(8.53333 * (currentBannerIndex + 1)) + "rem" }, 500);
+//         // foot
+//         footerList.removeClass("active").eq(currentBannerIndex + 1).addClass("active");
+//         // header滑动
+//         headerList.removeClass("active").eq(currentBannerIndex + 1).addClass("active");
 
-        if([1,2,3].indexOf(currentBannerIndex) >= 0){
-            header.animate({marginLeft: -(1.46667 * currentBannerIndex) + "rem"});
-        }
-        // 索引增加
-        currentBannerIndex++;
-    }
-});
+//         if([1,2,3].indexOf(currentBannerIndex) >= 0){
+//             header.animate({marginLeft: -(1.46667 * currentBannerIndex) + "rem"});
+//         }
+//         // 索引增加
+//         currentBannerIndex++;
+//     }
+// });
 
-// 右滑 banner向右走 margin-left 增大
-swipeListHammer.on("swiperight", function(e){
-    if(currentBannerIndex > 0){
-        banner.animate({marginLeft: -(8.53333 * (currentBannerIndex - 1)) + "rem" }, 500);
-        // foot
-        footerList.removeClass("active").eq(currentBannerIndex - 1).addClass("active");
-        // header滑动
-        headerList.removeClass("active").eq(currentBannerIndex - 1).addClass("active");
+// // 右滑 banner向右走 margin-left 增大
+// swipeListHammer.on("swiperight", function(e){
+//     if(currentBannerIndex > 0){
+//         banner.animate({marginLeft: -(8.53333 * (currentBannerIndex - 1)) + "rem" }, 500);
+//         // foot
+//         footerList.removeClass("active").eq(currentBannerIndex - 1).addClass("active");
+//         // header滑动
+//         headerList.removeClass("active").eq(currentBannerIndex - 1).addClass("active");
 
-        if([4,3,2].indexOf(currentBannerIndex) >= 0){
-            header.animate({marginLeft: -(1.46667 * (currentBannerIndex - 2)) + "rem"});
-        }
-        currentBannerIndex--;
-    }
-});
+//         if([4,3,2].indexOf(currentBannerIndex) >= 0){
+//             header.animate({marginLeft: -(1.46667 * (currentBannerIndex - 2)) + "rem"});
+//         }
+//         currentBannerIndex--;
+//     }
+// });
 // step 2 完成
+
+// step 2 中为了保存数据 减少查询DOM次数 使用了几个全局变量
+// 这样的话 污染全局变量 变量挂载在window对象上 很容易被人直接修改 这显然是不好的
+// 解决办法：改变变量的scope 立即执行函数/使用闭包
+
+// step 3 立即执行函数/使用闭包
+// 立即执行函数
+// (function () {
+//     var swipeListHammer = new Hammer($("#swipe-list")[0]);
+//     var header = $(".header-list");
+//     var headerList = $(".header-list li").hammer();
+//     var footerList = $(".foot span");
+//     var banner = $("#swipe-list");
+//     var currentBannerIndex = 0; // 记录当前显示的那个tab
+
+//     // 点击滑动
+//     headerList.on("tap", function(){
+//         var index = $(this).index();
+
+//         banner.animate({marginLeft: -(8.53333 * index) + "rem" }, 500);
+
+//         footerList.removeClass("active").eq(index).addClass("active");
+
+//         headerList.removeClass("active").eq(index).addClass("active");
+
+//         if([2,3,4].indexOf(index) >= 0){
+//             header.animate({marginLeft: -(1.46667 * (index - 1)) + "rem"});
+//         }
+//         currentBannerIndex = index;
+//     });
+//     // ←滑 banner向左走 margin-left 减小
+//     swipeListHammer.on("swipeleft", function(e){
+//         // 最后一张 不可再右滑 所以是<5
+//         if(currentBannerIndex < 5){
+//             banner.animate({marginLeft: -(8.53333 * (currentBannerIndex + 1)) + "rem" }, 500);
+//             // foot
+//             footerList.removeClass("active").eq(currentBannerIndex + 1).addClass("active");
+//             // header滑动
+//             headerList.removeClass("active").eq(currentBannerIndex + 1).addClass("active");
+
+//             if([1,2,3].indexOf(currentBannerIndex) >= 0){
+//                 header.animate({marginLeft: -(1.46667 * currentBannerIndex) + "rem"});
+//             }
+//             // 索引增加
+//             currentBannerIndex++;
+//         }
+//     });
+
+//     // 右滑 banner向右走 margin-left 增大
+//     swipeListHammer.on("swiperight", function(e){
+//         if(currentBannerIndex > 0){
+//             banner.animate({marginLeft: -(8.53333 * (currentBannerIndex - 1)) + "rem" }, 500);
+//             // foot
+//             footerList.removeClass("active").eq(currentBannerIndex - 1).addClass("active");
+//             // header滑动
+//             headerList.removeClass("active").eq(currentBannerIndex - 1).addClass("active");
+
+//             if([4,3,2].indexOf(currentBannerIndex) >= 0){
+//                 header.animate({marginLeft: -(1.46667 * (currentBannerIndex - 2)) + "rem"});
+//             }
+//             currentBannerIndex--;
+//         }
+//     });
+// })();
+
+// 闭包
+var swipeClosure = (function () {
+    var swipeListHammer = new Hammer($("#swipe-list")[0]);
+    var header = $(".header-list");
+    var headerList = $(".header-list li").hammer();
+    var footerList = $(".foot span");
+    var banner = $("#swipe-list");
+    var currentBannerIndex = 0; // 记录当前显示的那个tab
+
+    return function(){
+        // 点击滑动
+        headerList.on("tap", function(){
+            var index = $(this).index();
+
+            banner.animate({marginLeft: -(8.53333 * index) + "rem" }, 500);
+
+            footerList.removeClass("active").eq(index).addClass("active");
+
+            headerList.removeClass("active").eq(index).addClass("active");
+
+            if([2,3,4].indexOf(index) >= 0){
+                header.animate({marginLeft: -(1.46667 * (index - 1)) + "rem"});
+            }
+            currentBannerIndex = index;
+        });
+        // ←滑 banner向左走 margin-left 减小
+        swipeListHammer.on("swipeleft", function(e){
+            // 最后一张 不可再右滑 所以是<5
+            if(currentBannerIndex < 5){
+                banner.animate({marginLeft: -(8.53333 * (currentBannerIndex + 1)) + "rem" }, 500);
+                // foot
+                footerList.removeClass("active").eq(currentBannerIndex + 1).addClass("active");
+                // header滑动
+                headerList.removeClass("active").eq(currentBannerIndex + 1).addClass("active");
+
+                if([1,2,3].indexOf(currentBannerIndex) >= 0){
+                    header.animate({marginLeft: -(1.46667 * currentBannerIndex) + "rem"});
+                }
+                // 索引增加
+                currentBannerIndex++;
+            }
+        });
+
+        // 右滑 banner向右走 margin-left 增大
+        swipeListHammer.on("swiperight", function(e){
+            if(currentBannerIndex > 0){
+                banner.animate({marginLeft: -(8.53333 * (currentBannerIndex - 1)) + "rem" }, 500);
+                // foot
+                footerList.removeClass("active").eq(currentBannerIndex - 1).addClass("active");
+                // header滑动
+                headerList.removeClass("active").eq(currentBannerIndex - 1).addClass("active");
+
+                if([4,3,2].indexOf(currentBannerIndex) >= 0){
+                    header.animate({marginLeft: -(1.46667 * (currentBannerIndex - 2)) + "rem"});
+                }
+                currentBannerIndex--;
+            }
+        });
+    }
+})();
+swipeClosure();
+
+// 推荐使用闭包 因为闭包相对于立即执行函数更可控 不需要的时候 可以删除等
+// step 3 完成
+
+
